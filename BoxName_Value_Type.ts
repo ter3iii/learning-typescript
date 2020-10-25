db3792200eadb0016d51bae6717528bc122e4a7f
ter3iii <73420532+ter3iii@users.noreply.github.com>
2020-10-25 19:47:19 +0800

    Update BoxName_Value_Type.ts

--- BoxName_Value_Type.ts
+++ BoxName_Value_Type.ts
@@ -7,3 +7,10 @@ let B: string= "hihi";
   console.log (B);
 console.log (size_of_array);
 console.log (a_size_of_array);
+
+
+let v: string | null = window.prompt("What is your name");
+let y: string | null = window.prompt("How old are you?");
+let a: string | null = window.prompt("Where are you live");
+let b: string | null = window.prompt("What is your favourite food")
+console.log("Hello  "+ v  + "  you are  " + y + "  years old. "+ " You are living in  " + a + " Your favourite food is "+ b)
