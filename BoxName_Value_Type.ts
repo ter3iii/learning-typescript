644d6fda2f0e3bd5353954dbc15cebedac3ded68
ter3iii <73420532+ter3iii@users.noreply.github.com>
2020-10-25 18:29:16 +0800

    Create BoxName_Value_Type.ts

---
+++ BoxName_Value_Type.ts
@@ -0,0 +1,9 @@
+let A: number[] = [1,2,3];
+let size_of_array: number []= [1,2,3];
+console.log (A);
+
+let B: string= "hihi";
+  let a_size_of_array: string []= ["hihi"];
+  console.log (B);
+console.log (size_of_array);
+console.log (a_size_of_array);
