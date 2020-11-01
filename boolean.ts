ccc76b2d3c64c541683308255695aa7e8ec39be0
ter3iii <73420532+ter3iii@users.noreply.github.com>
2020-11-01 18:24:11 +0800

    Create boolean.ts

---
+++ boolean.ts
@@ -0,0 +1,16 @@
+let a : number;
+let z: string;
+
+let yes : boolean= true;
+let no : boolean= false;
+console.log (yes)
+
+let age: number = 18
+if (age >= 20)
+{
+    console.log("Happy 20 life!");
+}
+else 
+{
+    console.log("ohhhh!");
+}
